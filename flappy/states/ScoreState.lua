023ccd8c63c45f470471562eb7f3bf2f7ba23ac3
Ádrian Gama <adrian.gama.se@gmail.com>
2024-05-24 17:32:06 -0300

    add medals based on score points

--- flappy/states/ScoreState.lua
+++ flappy/states/ScoreState.lua
@@ -10,12 +10,33 @@
 
 ScoreState = Class{__includes = BaseState}
 
+local medals = {
+    {
+        sprite = love.graphics.newImage('images/bronze-medal.png'),
+        minScore = 0
+    },
+    {
+        sprite = love.graphics.newImage('images/silver-medal.png'),
+        minScore = 5
+    },
+    {
+        sprite = love.graphics.newImage('images/gold-medal.png'),
+        minScore = 10
+    },
+}
+
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
 ]]
 function ScoreState:enter(params)
     self.score = params.score
+    -- select medal based on score
+    for _, medal in ipairs(medals) do
+        if self.score >= medal.minScore then
+            self.medal = medal.sprite
+        end
+    end
 end
 
 function ScoreState:update(dt)
@@ -29,6 +50,8 @@ function ScoreState:render()
     -- simply render the score to the middle of the screen
     love.graphics.setFont(flappyFont)
     love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.draw(self.medal, VIRTUAL_WIDTH / 2 - self.medal:getWidth() / 2, 120)
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
