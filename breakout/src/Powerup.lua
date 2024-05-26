613b0a63417ef473c1ca6558ed334d59eb25a202
Ádrian Gama <adrian.gama.se@gmail.com>
2024-05-25 23:13:16 -0300

    add duplicate ball a powerup

--- breakout/src/Powerup.lua
+++ breakout/src/Powerup.lua
@@ -1,5 +1,18 @@
 Powerup = Class{}
 
+local actions = {
+    -- duplicate a random ball
+    [9] = function (state)
+        local extra_ball = Ball(math.random(7))
+        local selected_ball = state.balls[math.random(#state.balls)]
+        extra_ball.x = selected_ball.x
+        extra_ball.y = selected_ball.y
+        extra_ball.dx = selected_ball.dx
+        extra_ball.dy = selected_ball.dy
+        table.insert(state.balls, extra_ball)
+    end
+}
+
 function Powerup:init(x, y, type)
     -- spawn point
     self.x = x
@@ -18,6 +31,10 @@ end
 
 function Powerup:update(dt)
   self.y = self.y + self.dy * dt
+end
+
+function Powerup:pickup(state)
+  actions[self.type](state)
 end
 
 function Powerup:render()
