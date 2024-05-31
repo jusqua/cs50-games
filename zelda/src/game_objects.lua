6570bdccda92d031f91cd1c479e9666f78c3fff9
Ádrian Gama <adrian.gama.se@gmail.com>
2024-05-31 11:45:34 -0300

    add pot game object

--- zelda/src/game_objects.lua
+++ zelda/src/game_objects.lua
@@ -33,6 +33,11 @@ GAME_OBJECT_DEFS = {
         solid = false
     },
     ['pot'] = {
-        -- TODO
+        type = 'pot',
+        texture = 'tiles',
+        frame = 14,
+        width = 16,
+        height = 16,
+        solid = true
     }
 }
