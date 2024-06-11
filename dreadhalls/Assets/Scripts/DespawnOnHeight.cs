210f61b8d5511384abe812e71c6825214a719827
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-10 21:36:58 -0300

    add game over scene and transition

---
+++ dreadhalls/Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour
+{
+    public int height;
+    private CharacterController characterController;
+
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        if (characterController.transform.position.y < height) {
+            Destroy(DontDestroy.instance.gameObject);
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
--- dreadhalls/Assets/Scripts/LoadSceneOnInput.cs
+++ dreadhalls/Assets/Scripts/LoadSceneOnInput.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadSceneOnInput : MonoBehaviour {
+	public String scene;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +15,7 @@ public class LoadSceneOnInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetAxis("Submit") == 1) {
-			SceneManager.LoadScene("Play");
+			SceneManager.LoadScene(scene);
 		}
 	}
 }
